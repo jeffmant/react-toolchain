46d24532fcc99594e41166470020e4c5ca64a5c1
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-03-10 13:40:54 -0300

    feat: improve hello world page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,36 @@ import React, { Component} from "react";
 import { hot } from "react-hot-loader";
 import "./App.css";
 
-class App extends Component{
+const ITEMS = [
+  { label: "React", url: "https://reactjs.org/", img: "" },
+  { label: "Webpack", url: "https://webpack.js.org/concepts/", img: "https://tinyurl.com/bdf3d3h6" },
+  { label: "Babel", url: "https://babeljs.io/docs/", img: "https://tinyurl.com/msdwphkn" }
+]
+
+class App extends Component {
   render(){
     return(
       <div className="App">
-        <h1> React custom toolchain </h1>
+        <div className="Card">
+          
+          <h3 className="CardTitle">
+            React Toolchain
+          </h3> 
+          
+          <hr className="hr" /> 
+
+          <ul className="List">
+            {
+              ITEMS.map((item, key) => (
+                <li key={key} className="ListItem">
+                  <a href={item.url} target="_blank">
+                    <img src={item.img} alt={`${item.label}`} />
+                  </a>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
       </div>
     );
   }
