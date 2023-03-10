f80c7292a6425fea0f2002be4d68e67fc2024551
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-03-10 16:09:14 -0300

    feat: add standard linter

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React, { Component} from "react";
-import "./App.css";
+import React, { Component } from 'react'
+import './App.css'
 
 const ITEMS = [
-  { label: "React", url: "https://reactjs.org/", img: "" },
-  { label: "Webpack", url: "https://webpack.js.org/concepts/", img: "https://tinyurl.com/bdf3d3h6" },
-  { label: "Babel", url: "https://babeljs.io/docs/", img: "https://tinyurl.com/msdwphkn" }
+  { label: 'React', url: 'https://reactjs.org/', img: '' },
+  { label: 'Webpack', url: 'https://webpack.js.org/concepts/', img: 'https://tinyurl.com/bdf3d3h6' },
+  { label: 'Babel', url: 'https://babeljs.io/docs/', img: 'https://tinyurl.com/msdwphkn' }
 ]
 
 class App extends Component {
-  render(){
-    return(
-      <div className="App">
-        <div className="Card">
-          
-          <h3 className="CardTitle">
-            React Toolchain
-          </h3> 
-          
-          <hr className="hr" /> 
+  render () {
+    return (
+      <div className='App'>
+        <div className='Card'>
 
-          <ul className="List">
+          <h3 className='CardTitle'>
+            React Toolchain
+          </h3>
+
+          <hr className='hr' />
+
+          <ul className='List'>
             {
               ITEMS.map((item, key) => (
-                <li key={key} className="ListItem">
-                  <a href={item.url} target="_blank">
+                <li key={key} className='ListItem'>
+                  <a href={item.url} target='_blank' rel='noreferrer'>
                     <img src={item.img} alt={`${item.label}`} />
                   </a>
                 </li>
@@ -32,8 +32,8 @@ class App extends Component {
           </ul>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  entry: './src/index.js',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
   },
   devServer: {
-    port: 3000  
-},
+    port: 3000
+  },
   plugins: [new webpack.HotModuleReplacementPlugin()]
-};
+}
